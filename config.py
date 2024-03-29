e0d23ac742ffbe2ff4ff12ee821022fffcb37123
Chacha song <chacha@wonder4studio.com>
2024-03-29 11:58:31 +0800

    update my config

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 
 # Get a bot token from botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7053774586:AAEjGIkdY2LEEo5EfWg5EAVNbAv91izb-zo")
 
 # Get from my.telegram.org (or @UseTGXBot)
-APP_ID = int(os.environ.get("APP_ID", ""))
+APP_ID = int(os.environ.get("APP_ID", "22316508"))
 
 # Get from my.telegram.org (or @UseTGXBot)
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "18a658f7d0ce664fb68fff33e9e06699")
 
 
 # You Can Add More Vars If You Want
